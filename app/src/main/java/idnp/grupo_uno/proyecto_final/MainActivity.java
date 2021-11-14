0e160062073470fc3df92e2330fa6531029b4778
EleoRomario <eleo.romario@gmail.com>
2021-11-14 17:16:02 -0500

    fragmente de lista de eventos y avance del home con algunos botones

--- app/src/main/java/idnp/grupo_uno/proyecto_final/MainActivity.java
+++ app/src/main/java/idnp/grupo_uno/proyecto_final/MainActivity.java
@@ -11,31 +11,47 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import idnp.grupo_uno.proyecto_final.ui.events.EventsListFragment;
 import idnp.grupo_uno.proyecto_final.ui.register.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "main";
-    Button fragRegister;
+    ImageButton fragRegister;
+    ImageButton btnBack;
+    Button fragEventsList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragRegister = findViewById(R.id.btn_register);
+        fragRegister = findViewById(R.id.btn_login_register);
         fragRegister.setOnClickListener(this);
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(this);
+        fragEventsList = findViewById(R.id.btn_events_list);
+        fragEventsList.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v){
-        if(v.getId()==R.id.btn_register){
+        if(v.getId()==R.id.btn_back){
+            onBackPressed();
+        }
+        if(v.getId()==R.id.btn_login_register){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_register, new RegisterFragment()).commit();
             fragRegister.setVisibility(View.GONE);
+        }
+        if(v.getId()==R.id.btn_events_list){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_events, new EventsListFragment()).commit();
+            fragEventsList.setVisibility(View.GONE);
         }
     }
 
