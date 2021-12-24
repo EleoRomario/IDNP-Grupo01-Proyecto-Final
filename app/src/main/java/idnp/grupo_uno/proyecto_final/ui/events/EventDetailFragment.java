8d9443f0dfb40d2b7e6fa6cc9c211827753c8536
aroquega <aroquega@unsa.edu.pe>
2021-12-24 06:13:00 -0500

    Añade el mapa al detalle y navegación de abajo

--- app/src/main/java/idnp/grupo_uno/proyecto_final/ui/events/EventDetailFragment.java
+++ app/src/main/java/idnp/grupo_uno/proyecto_final/ui/events/EventDetailFragment.java
@@ -2,15 +2,25 @@ package idnp.grupo_uno.proyecto_final.ui.events;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import idnp.grupo_uno.proyecto_final.R;
 
-public class EventDetailFragment extends Fragment {
+public class EventDetailFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +29,27 @@ public class EventDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_event_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        Log.d("Main", "Compila por amor de dios");
+        if (mapFragment != null) {
+            Log.d("Main", "Compila por amor de dios");
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Log.d("Main", "Por la chucha tumar");
+        LatLng eventLocation = new LatLng(16.4090, 71.5375);
+        LatLngBounds bounds = new LatLngBounds(eventLocation, eventLocation);
+        googleMap.setLatLngBoundsForCameraTarget(bounds);
+        googleMap.addMarker(new MarkerOptions().position(eventLocation).title("Lugar del Evento"));
     }
 }
