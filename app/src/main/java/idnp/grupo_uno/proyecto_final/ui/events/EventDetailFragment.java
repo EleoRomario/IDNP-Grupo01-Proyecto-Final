771341aad955ed403ba22469cc0d86a95e433379
aroquega <aroquega@unsa.edu.pe>
2021-12-24 20:26:06 -0500

    añade gps

--- app/src/main/java/idnp/grupo_uno/proyecto_final/ui/events/EventDetailFragment.java
+++ app/src/main/java/idnp/grupo_uno/proyecto_final/ui/events/EventDetailFragment.java
@@ -1,5 +1,6 @@
 package idnp.grupo_uno.proyecto_final.ui.events;
 
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,16 +12,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.concurrent.Executor;
 
 import idnp.grupo_uno.proyecto_final.R;
 
 public class EventDetailFragment extends Fragment implements OnMapReadyCallback {
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,19 +45,33 @@ public class EventDetailFragment extends Fragment implements OnMapReadyCallback 
         super.onViewCreated(view, savedInstanceState);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        Log.d("Main", "Compila por amor de dios");
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
         if (mapFragment != null) {
-            Log.d("Main", "Compila por amor de dios");
             mapFragment.getMapAsync(this);
         }
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        Log.d("Main", "Por la chucha tumar");
-        LatLng eventLocation = new LatLng(16.4090, 71.5375);
-        LatLngBounds bounds = new LatLngBounds(eventLocation, eventLocation);
-        googleMap.setLatLngBoundsForCameraTarget(bounds);
+        LatLng eventLocation = new LatLng(-13.52264, -71.96734);
         googleMap.addMarker(new MarkerOptions().position(eventLocation).title("Lugar del Evento"));
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(@NonNull Location location) {
+                if (location != null) {
+                    LatLng myPosition = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    LatLngBounds bounds = LatLngBounds.builder()
+                            .include(eventLocation)
+                            .include(myPosition).build();
+
+                    googleMap.setLatLngBoundsForCameraTarget(bounds);
+
+                    googleMap.addMarker(new MarkerOptions().position(myPosition).title("Posición Actual"));
+                }
+            }
+        });
+
     }
 }
