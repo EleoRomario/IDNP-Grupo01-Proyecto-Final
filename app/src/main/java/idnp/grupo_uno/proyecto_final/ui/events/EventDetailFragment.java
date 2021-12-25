ed530c332941f696e036828d2a234b5a757a75bb
vladcw <vladcsw@gmail.com>
2021-12-24 20:02:44 -0500

    graficas

--- app/src/main/java/idnp/grupo_uno/proyecto_final/ui/events/EventDetailFragment.java
+++ app/src/main/java/idnp/grupo_uno/proyecto_final/ui/events/EventDetailFragment.java
@@ -1,5 +1,6 @@
 package idnp.grupo_uno.proyecto_final.ui.events;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.taufiqrahman.reviewratings.BarLabels;
+import com.taufiqrahman.reviewratings.RatingReviews;
+
+import java.util.Random;
 
 import idnp.grupo_uno.proyecto_final.R;
 
@@ -29,12 +34,34 @@ public class EventDetailFragment extends Fragment implements OnMapReadyCallback 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_event_detail, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        RatingReviews ratingReviews = (RatingReviews) getView().findViewById(R.id.rating_reviews);
+
+        int colors[] = new int[]{
+                Color.parseColor("#0e9d58"),
+                Color.parseColor("#bfd047"),
+                Color.parseColor("#ffc105"),
+                Color.parseColor("#ef7e14"),
+                Color.parseColor("#d36259")};
+
+        int raters[] = new int[]{
+                new Random().nextInt(15)+5,
+                new Random().nextInt(15)+5,
+                new Random().nextInt(10),
+                new Random().nextInt(4),
+                new Random().nextInt(4)
+        };
+
+
+        ratingReviews.createRatingBars(20, BarLabels.STYPE1, colors, raters);
+
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         Log.d("Main", "Compila por amor de dios");
